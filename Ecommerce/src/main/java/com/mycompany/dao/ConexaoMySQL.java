653618704a2d411753ef9145ee87f5a4cb6b274e
PedroMelo06 <pedrolucaslopesmelo@gmail.com>
2025-10-13 12:32:16 -0300

    Atualizando dados na classe ConexaoMySQL

--- Ecommerce/src/main/java/com/mycompany/dao/ConexaoMySQL.java
+++ Ecommerce/src/main/java/com/mycompany/dao/ConexaoMySQL.java
@@ -15,8 +15,8 @@ import java.sql.SQLException;
  */
 public class ConexaoMySQL implements ConexaoBD {
     private static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
-    private static final String USUARIO = "digite_seu_usuario"; 
-    private static final String SENHA = "digite_sua_senha"; 
+    private static final String USUARIO = "root"; 
+    private static final String SENHA = "*06032006Pedro"; 
     
     @Override
     public Connection obterConexao() throws Exception {
