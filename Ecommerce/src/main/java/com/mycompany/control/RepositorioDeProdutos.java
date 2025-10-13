cff763134e72ffba63bbc65550328d41b1c9add2
PedroMelo06 <pedrolucaslopesmelo@gmail.com>
2025-10-12 23:37:35 -0300

    Atualizando pacotes e .xml

---
+++ Ecommerce/src/main/java/com/mycompany/control/RepositorioDeProdutos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.control;
+
+import com.mycompany.model.Produto;
+import java.util.List;
+/**
+ *
+ * @author pedrolucas.melo
+ */
+public interface RepositorioDeProdutos {
+    
+    List<Produto> listarTodos() throws Exception;
+    
+}
---
+++ Ecommerce/src/main/java/com/mycompany/model/Produto.java
@@ -0,0 +1,87 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author pedrolucas.melo
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade_estoque;
+
+    public Produto(){
+        this.quantidade_estoque = 0; 
+    }
+    
+    public Produto(String nome){
+        this(); 
+        this.nome = nome;
+    }
+    
+    public Produto(String nome, String descricao){
+        this(nome); 
+        this.descricao = descricao;
+    }
+    
+    public Produto(String nome, String descricao, double preco){
+        this(nome, descricao); 
+        this.preco = preco;
+    }
+    
+    public Produto(int id, String nome, String descricao, double preco, int quantidade_estoque){
+        this(nome, descricao, preco); 
+        this.id = id;
+        this.quantidade_estoque = quantidade_estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade_estoque() {
+        return quantidade_estoque;
+    }
+
+    public void setQuantidade_estoque(int quantidade_estoque) {
+        this.quantidade_estoque = quantidade_estoque;
+    }
+
+}
